b091d5ee76a1e26d896a5664e98caa31efdb791a
Yadier <yadierg7@gmail.com>
2023-12-18 19:38:17 -0400

    added comments for better understanding

--- app/components/ProductRequest.tsx
+++ app/components/ProductRequest.tsx
@@ -12,15 +12,35 @@ export const ProductRequest = ({
   incrementUpvotes,
 }: ProductRequestProps) => {
   return (
-    <div className="flex gap-5 p-10 flex-col text-sm">
-      <div className="flex  flex-col gap-2">
-        <p className="text-[#3a4374] font-bold">{item.title}</p>
-        <p className="text-[#647196]">{item.description}</p>
-        <p className="w-fit px-3 py-1 rounded-lg capitalize text-[#4661e6] font-bold text-xs bg-[#f2f4ff]">
-          {item.category}
-        </p>
+    <div className="flex gap-5 p-8 flex-col text-sm w-full">
+      <div className="flex  flex-col md:flex-row  gap-2 justify-between">
+        <div className="flex">
+          {/* upvotes button hidden normal  flex if md */}
+          <button
+            onClick={() => incrementUpvotes(item.id)}
+            className="md:flex  h-fit rounded-lg gap-2 bg-[#f2f4ff] px-2  py-2 w-14  hidden    items-center bg-[#f2f4fftext-[#647196]"
+          >
+            <SvgsArrowUp />
+            <p className="text-xs font-bold "> {item.upvotes}</p>
+          </button>
+          {/*  */}
+          <span className="ml-4 flex flex-col gap-2">
+            <p className="text-[#3a4374] font-bold">{item.title}</p>
+            <p className="text-[#647196]">{item.description}</p>
+            <p className="w-fit px-3 py-1 rounded-lg capitalize text-[#4661e6] font-bold text-xs bg-[#f2f4ff]">
+              {item.category}
+            </p>
+          </span>
+        </div>
+
+        <span className="md:flex items-center gap-2 hidden ">
+          <SvgsComments />
+          <p className="text-[#3a4374] font-bold ">{item.comments.length}</p>
+        </span>
       </div>
-      <div className="flex items-center justify-between text-sm ">
+
+      {/* upvotes & Svgcomments if md:hidden */}
+      <div className="flex items-center justify-between text-sm md:hidden ">
         <button
           onClick={() => incrementUpvotes(item.id)}
           className="flex  h-fit rounded-lg gap-2 bg-[#f2f4ff] px-2  py-2 w-14      items-center bg-[#f2f4fftext-[#647196]]"
