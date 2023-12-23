c45bc3946c2270123bc184490dfd1e2df9e10748
Yadier <yadierg7@gmail.com>
2023-12-22 21:20:20 -0400

    updated UI

--- app/feedback/[id]/page.tsx
+++ app/feedback/[id]/page.tsx
@@ -3,6 +3,7 @@ import { Comment } from "../../components/Comment";
 import { ProductRequest } from "@/app/components/ProductRequest";
 import Link from "next/link";
 import { NoComment } from "@/app/components/NoComment";
+import { it } from "node:test";
 
 export default function page({ params }: { params: { id: number } }) {
   const item = data.productRequests.find((item) => item.id == params.id);
@@ -10,27 +11,27 @@ export default function page({ params }: { params: { id: number } }) {
   console.log(item?.comments.length === 0);
 
   return (
-    <>
-      <main className="w-screen flex  justify-center  bg-[#f7f8fd]  items-center">
-        <div className="flex max-w-screen-lg gap-4 h-screen p-7 flex-col   w-full ">
-          <div className="flex justify-between">
-            <Link href="/">Go Back</Link>
-            <Link href={`/feedback/${item?.id}/edit/${item?.id}`}>Edit</Link>
-          </div>
-          <div className="bg-[#ffffff] ">
-            <ProductRequest item={item} />
-          </div>
-          <div className="bg-white p-6 lg:p-8">
-            {item?.comments.length === 0 ? (
-              <NoComment />
-            ) : (
-              item?.comments.map((comment) => (
-                <Comment comment={comment} key={comment.id} />
-              ))
-            )}
-          </div>
+    <main className="w-screen flex min-h-screen justify-center  bg-[#f7f8fd]  items-center">
+      <div className="flex  max-w-screen-md gap-4 h-full p-7 flex-col   w-full ">
+        <div className="flex justify-between">
+          <Link href="/">Go Back</Link>
+          <Link href={`/feedback/${item?.id}/edit/${item?.id}`}>Edit</Link>
         </div>
-      </main>
-    </>
+        <div className="bg-[#ffffff] ">
+          <ProductRequest item={item} />
+        </div>
+        <div className="flex flex-col gap-6 p-8  bg-white   rounded-lg">
+          {
+            <h3 className="font-bold text-lg">
+              {item?.comments.length} Comment
+            </h3>
+          }
+          {item?.comments.map((comment) => (
+            <Comment comment={comment} key={comment.id} />
+          ))}
+        </div>
+        <NoComment />
+      </div>
+    </main>
   );
 }
