d9556b58db333fffed133abaa09604ac929405f9
Yadier <yadierg7@gmail.com>
2023-12-22 20:08:32 -0400

    Change the conditional rendering of characters

--- app/components/NoComment.tsx
+++ app/components/NoComment.tsx
@@ -1,16 +1,36 @@
+"use client";
+import { set } from "mongoose";
+import { useState } from "react";
 export const NoComment = () => {
+  const [userComment, setUserComment] = useState("");
+  const [charLenght, setCharLenght] = useState(255);
+  const inputHandler = (e: any) => {
+    if (e.target.value.length > 256) return;
+
+    if (e.nativeEvent.inputType === "deleteContentBackward") {
+      setCharLenght(charLenght + 1);
+    } else {
+      setCharLenght(charLenght - 1);
+    }
+
+    setUserComment(e.target.value);
+  };
   return (
-    <div>
+    <div className="flex flex-col gap-6">
       <p>Add Comment</p>
       <textarea
         name=""
+        value={userComment}
+        onChange={inputHandler}
         id=""
         className="w-full h-20 p-4  bg-[#f7f8fd]"
         placeholder="Type your comment here"
       ></textarea>
-      <div className="flex justify-between">
-        <p>255 characters left</p>
-        <button>Post Comment</button>
+      <div className="flex justify-between  items-center mt-2">
+        <p>{charLenght} characters left</p>
+        <button className="bg-[#ad1fea] font-bold text-sm text-white p-3 px-7 rounded-xl">
+          Post Comment
+        </button>
       </div>
     </div>
   );
