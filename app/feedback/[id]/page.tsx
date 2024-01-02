e28173945ed26f9c4a00a44621c8b1e775f37c79
Yadier <yadierg7@gmail.com>
2024-01-02 16:55:10 -0400

    changed page feedback/:id UI

--- app/feedback/[id]/page.tsx
+++ app/feedback/[id]/page.tsx
@@ -1,33 +1,13 @@
 import data from "../../data.json";
-import { Comment } from "../../components/Comment";
-import { ProductRequest } from "@/app/components/ProductRequest";
-import Link from "next/link";
 import { NoComment } from "@/app/components/NoComment";
+import { ProductPage } from "@/app/components/ProductPage";
 
 export default function page({ params }: { params: { id: number } }) {
-  const item = data.productRequests.find((item) => item.id == params.id);
-
   return (
     <main className="w-screen flex min-h-screen justify-center  bg-[#f7f8fd]  items-center">
       <div className="flex  max-w-screen-md gap-4 h-full p-7 flex-col   w-full ">
-        <div className="flex justify-between">
-          <Link href="/">Go Back</Link>
-          <Link href={`/feedback/${item?.id}/edit/${item?.id}`}>Edit</Link>
-        </div>
         <div className="bg-[#ffffff] ">
-          <ProductRequest item={item} />
-        </div>
-        <div className="flex flex-col gap-6 p-8  bg-white   rounded-lg">
-          {
-            <h3 className="font-bold text-lg">
-              {item?.comments.length} Comment
-            </h3>
-          }
-          {item?.comments.map((comment) => (
-            <>
-              <Comment comment={comment} key={comment.id} />
-            </>
-          ))}
+          <ProductPage params={params} />
         </div>
 
         <NoComment />
